c16ab01e1b4a8660f2e88e0328927f2b1ee1bc16
Marcian Diamond <marciandmnd@yahoo.com>
2016-12-07 15:45:03 +0100

    added login view and menu for anonymous or authenticated users

---
+++ routes/login.js
@@ -0,0 +1,29 @@
+var User = require('../lib/user');
+
+exports.form = function(req, res) {
+  res.render('login', {title: 'Login'});
+};
+
+exports.submit = function(req, res, next) {
+  // var data = request.body.user;
+  var name = request.body.name;
+  var pass = request.body.pass;
+
+  User.authenticate(name, pass, function(err, user){
+    if (err) return next(err);
+    if (user) {
+      req.session.uid = user.id;
+      res.redirect('/');
+    } else {
+      res.error("Sorry! Invalid credentials.");
+      res.redirect('back');
+    }
+  });
+}
+
+exports.logout = function(req, res) {
+  req.session.destroy(function(err){
+    if (err) throw err;
+    res.redirect('/');
+  });
+}
